d14a214baa336f789be4b7dca781c307b2c76274
internshalagit <rohitnadageri007@gmail.com>
2024-08-17 21:38:27 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardSection from "./components/CardSection";
 import ChartSection from "./components/ChartSection";
 import Header from "./components/Header";
+import LoginSignup from "./components/LoginSignup";
 
 export default class App extends Component {
   constructor() {
@@ -9,96 +10,112 @@ export default class App extends Component {
     this.state = {
       Id: "bitcoin",
       Data: {},
+      isAuthenticated: false,
     };
   }
+
   fetchData = async () => {
     let data = await fetch(
       "https://api.coingecko.com/api/v3/coins/" + this.state.Id
     );
     let JsonData = await data.json();
-    this.setState({ Id: this.state.Id, Data: JsonData });
+    this.setState({ Data: JsonData });
   };
 
   handleSubmit = async (event) => {
-    console.log(event.target.value);
-    await this.setState({ Id: event.target.value, Data: this.state.Data });
+    await this.setState({ Id: event.target.value });
     this.fetchData();
   };
 
+  handleAuth = (isAuthenticated) => {
+    this.setState({ isAuthenticated });
+  };
+
   componentDidMount() {
-    this.fetchData();
-    this.interval = setInterval(() => this.fetchData(), 2000);
+    if (this.state.isAuthenticated) {
+      this.fetchData();
+      this.interval = setInterval(() => this.fetchData(), 2000);
+    }
   }
+
   componentWillUnmount() {
-    clearInterval(this.interval);
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
   }
 
   render() {
     return (
       <div>
-        <Header handle_Submit={this.handleSubmit} />
-        <CardSection
-          coinName={this.state.Data.name}
-          currentPrice={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.current_price["usd"]
-              : ""
-          }
-          mCap24={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.market_cap_change_percentage_24h
-              : ""
-          }
-          ath={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.ath.usd
-              : ""
-          }
-          atl={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.ath.usd
-              : ""
-          }
-          sentiment={this.state.Data.sentiment_votes_up_percentage}
-          high24={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.high_24h["usd"]
-              : ""
-          }
-          low24={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.low_24h["usd"]
-              : ""
-          }
-        />
-        <ChartSection
-          Id={this.state.Id}
-          priceChange24={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.price_change_24h_in_currency.usd
-              : ""
-          }
-          MarketCap={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.market_cap.usd
-              : ""
-          }
-          TotVol={
-            this.state.Data.market_data
-              ? this.state.Data.market_data.total_volume.usd
-              : ""
-          }
-          Circulating={
-            this.state.Data.market_data
-              ? this.state.Data.market_data["circulating_supply"]
-              : ""
-          }
-          twitterF={
-            this.state.Data.community_data
-              ? this.state.Data.community_data.twitter_followers
-              : ""
-          }
-        />
+        {this.state.isAuthenticated ? (
+          <>
+            <Header handle_Submit={this.handleSubmit} />
+            <CardSection
+              coinName={this.state.Data.name}
+              currentPrice={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.current_price["usd"]
+                  : ""
+              }
+              mCap24={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.market_cap_change_percentage_24h
+                  : ""
+              }
+              ath={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.ath.usd
+                  : ""
+              }
+              atl={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.ath.usd
+                  : ""
+              }
+              sentiment={this.state.Data.sentiment_votes_up_percentage}
+              high24={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.high_24h["usd"]
+                  : ""
+              }
+              low24={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.low_24h["usd"]
+                  : ""
+              }
+            />
+            <ChartSection
+              Id={this.state.Id}
+              priceChange24={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.price_change_24h_in_currency.usd
+                  : ""
+              }
+              MarketCap={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.market_cap.usd
+                  : ""
+              }
+              TotVol={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data.total_volume.usd
+                  : ""
+              }
+              Circulating={
+                this.state.Data.market_data
+                  ? this.state.Data.market_data["circulating_supply"]
+                  : ""
+              }
+              twitterF={
+                this.state.Data.community_data
+                  ? this.state.Data.community_data.twitter_followers
+                  : ""
+              }
+            />
+          </>
+        ) : (
+          <LoginSignup onAuth={this.handleAuth} />
+        )}
       </div>
     );
   }
---
+++ src/components/LoginSignup.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+function LoginSignup({ onAuth }) {
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Hardcoded users for demonstration
+  const users = [
+    { email: "user1@example.com", password: "password1" },
+    { email: "user2@example.com", password: "password2" },
+    { email: "user3@example.com", password: "password3" },
+  ];
+
+  const handleAuth = (e) => {
+    e.preventDefault();
+    const validUser = users.find(
+      (user) => user.email === email && user.password === password
+    );
+    if (validUser) {
+      onAuth(true);
+    } else {
+      alert("Invalid credentials");
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center", padding: "50px" }}>
+      <div
+        style={{
+          width: "300px",
+          padding: "20px",
+          border: "1px solid #ccc",
+          borderRadius: "10px",
+        }}
+      >
+        <h2>{isLogin ? "Login" : "Sign Up"}</h2>
+        <form onSubmit={handleAuth}>
+          <div style={{ marginBottom: "15px" }}>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              style={{ width: "100%", padding: "10px", marginTop: "5px" }}
+            />
+          </div>
+          <div style={{ marginBottom: "15px" }}>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              style={{ width: "100%", padding: "10px", marginTop: "5px" }}
+            />
+          </div>
+          <button
+            type="submit"
+            style={{
+              padding: "10px 20px",
+              backgroundColor: "#007bff",
+              color: "#fff",
+              border: "none",
+              borderRadius: "5px",
+            }}
+          >
+            {isLogin ? "Login" : "Sign Up"}
+          </button>
+        </form>
+        <p
+          style={{ marginTop: "10px", cursor: "pointer", color: "blue" }}
+          onClick={() => setIsLogin(!isLogin)}
+        >
+          {isLogin
+            ? "Don't have an account? Sign Up"
+            : "Already have an account? Login"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginSignup;
